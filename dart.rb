0b645848522212480d5673f26732bd1adde7a243
Justin Rhude <justin.rhude@workiva.com>
2020-10-26 17:00:44 -0400

    remove dev version from tap

--- dart.rb
+++ dart.rb
@@ -24,30 +24,6 @@ class Dart < Formula
       end
     end
   
-    devel do
-      version "2.8.0-dev.16.0"
-      if OS.mac?
-        url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.8.0-dev.16.0/sdk/dartsdk-macos-x64-release.zip"
-        sha256 "a3d07d2ff577b6b01d9b943801319ad3e332b2946f3790cc13205c3c9bab4c17"
-      elsif OS.linux? && Hardware::CPU.intel?
-        if Hardware::CPU.is_64_bit?
-          url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.8.0-dev.16.0/sdk/dartsdk-linux-x64-release.zip"
-          sha256 "108ccc27d181cbdced91be78163d9c8d0ca876654f46bae99faaad27c20c3fde"
-        else
-          url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.8.0-dev.16.0/sdk/dartsdk-linux-ia32-release.zip"
-          sha256 "5dcab790578a1bb3e568daca167eeeeb225f4fff5a85d16d3aab5216b9b62c85"
-        end
-      elsif OS.linux? && Hardware::CPU.arm?
-        if Hardware::CPU.is_64_bit?
-          url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.8.0-dev.16.0/sdk/dartsdk-linux-arm64-release.zip"
-          sha256 "6e06354531f3bb14deee7cb623bfb64e95cdc9363d4677f1f342772610426974"
-        else
-          url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.8.0-dev.16.0/sdk/dartsdk-linux-arm-release.zip"
-          sha256 "34109b0f998dc1d0dce075d93ef67827f4c400b4c59e4e437d5dc9c65c0527d4"
-        end
-      end
-    end
-  
     def install
       libexec.install Dir["*"]
       bin.install_symlink "#{libexec}/bin/dart"
